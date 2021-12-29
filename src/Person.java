22cba81170d523927ea48bac0dde543e1d064f46
benny-fields3rd <bfields3@gmail.com>
2021-12-28 21:44:03 -0600

    Added Person.java and completed exercises.

--- src/Person.java
+++ src/Person.java
@@ -47,16 +47,18 @@ public class Person {
 // 2. Understanding references
 // The following code blocks make use of the Person class from the previous exercise. For each code block, think about
 // what the code will output, then copy the code into IntelliJ and run it to see if it matches your expectations.
-
+        System.out.println("Testing if person1 is equal to person2: ");
         Person person1 = new Person("John"); // assigns John to person1
         Person person2 = new Person("John"); // assigns John to person2
-        System.out.println(person1.getName().equals(person2.getName())); // true for value being equal
-        System.out.println(person1 == person2); // false for different reference type (2 different people named John)
+        System.out.println(person1.getName().equals(person2.getName()) + ": for if equal in value"); // true for value being equal
+        System.out.println((person1 == person2) + ": for if equal in ref type.\n" ); // false for different reference type (2 different people named John)
 
+        System.out.println("Testing if person3 is equal to person4: ");
         Person person3 = new Person("John"); // assigns John to person3
         Person person4 = person3; // assigns value (John) also to person4
-        System.out.println(person3 == person4); // true because values are same
+        System.out.println((person3 == person4) + ": for if equal in value and ref type.\n"); // true because values are same
 
+        System.out.println("Testing values of person5 and person6: ");
         Person person5 = new Person("John"); // assign John to person5
         Person person6 = person5; // assigns value of person5 to person 6
         System.out.println(person5.getName()); // prints John
