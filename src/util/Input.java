1b7bdce4bae55b75572910bd7b09ea1c00827ff4
benny-fields3rd <bfields3@gmail.com>
2021-12-29 22:26:40 -0600

    Added package util

---
+++ src/util/Input.java
@@ -0,0 +1,29 @@
+package util;
+
+
+// 1. Create an input validation class
+//Create a package inside of src named util. Inside of util, create a class named Input that has a private field named
+// scanner. When an instance of this object is created, the scanner field should be set to a new instance of the
+// Scanner class. The class should have the following methods, all of which return command line input from the user:
+
+//      - String getString()
+//      - boolean yesNo()
+//      - int getInt(int min, int max)
+//      - int getInt()
+//      - double getDouble(double min, double max)
+//      - double getDouble()
+
+//The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
+
+//The getInt(int min, int max) method should keep prompting the user for input until they give an integer within the
+// min and max. The getDouble method should do the same thing, but with decimal numbers.
+
+//Create another class named InputTest that has a static main method that uses all the methods from the Input class.
+
+//Bonus
+//Allow all of your methods for getting input to accept an optional String parameter named prompt. If passed, the
+// method should show the given prompt to the user before parsing the input.
+public class Input {
+
+
+}
